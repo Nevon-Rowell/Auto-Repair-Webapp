a0f834a3b84cdef1d3c96c566f736366fcc36cc8
Nevon-Rowell <it25200166@my.sliit.lk>
2026-08-18 15:56:35 +0530

    Starting point

--- models/Job.java
+++ models/Job.java
@@ -1,11 +1,19 @@
 package models
 @Entity
 public class Job{
-    @Id
+    @Id @GeneratedValue
     private int jobID;
     private String jobStatus;
     private String jobNotes;
     private String leadMech;
+
+    public int getJobID() {
+        return jobID;
+    }
+
+    public void setJobID(int jobID) {
+        this.jobID = jobID;
+    }
 
     public void setJobNotes(String jobNotes) {
         this.jobNotes = jobNotes;
